b0afc123570327e1e2a09d02c3ba115ac4919f37
Akhil Kanawade <yashkanawade66@gmail.com>
2026-03-12 19:10:04 +0530

    added auth dependencies

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const port = process.env.PORT || 8080;
 const userModel = require("./models/user");
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log("Connection Error:", err));
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log("Connection Error:", err));
 
 app.set("view engine", "ejs");
 
@@ -27,33 +27,42 @@ app.listen(port, () => {
 });
 
 
-function isLoggedIn(req, res, next){
-    if(!req.cookies.token){
+function isLoggedIn(req, res, next) {
+
+    if (!req.cookies.token) {
         return res.redirect("/login");
     }
 
-    let data = jwt.verify(req.cookies.token,"secretkey");
-    req.user = data;
-    next();
+    try {
+        let data = jwt.verify(req.cookies.token, "secretkey");
+        req.user = data;
+        next();
+    }
+    catch (err) {
+        res.redirect("/login");
+    }
 }
 
 
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.redirect("/login");
 });
 
 
-app.get("/register",(req,res)=>{
+//register
+
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
 
-app.post("/register", async (req,res)=>{
+app.post("/register", async (req, res) => {
 
-    let {name,email,password,image} = req.body;
+    let { name, email, password, image } = req.body;
 
     let salt = await bcrypt.genSalt(10);
-    let hash = await bcrypt.hash(password,salt);
+    let hash = await bcrypt.hash(password, salt);
 
     await userModel.create({
         name,
@@ -66,46 +75,60 @@ app.post("/register", async (req,res)=>{
 });
 
 
-app.get("/login",(req,res)=>{
+//login
+
+app.get("/login", (req, res) => {
     res.render("login");
 });
 
 
-app.post("/login", async (req,res)=>{
+app.post("/login", async (req, res) => {
 
-    let user = await userModel.findOne({email:req.body.email});
+    let user = await userModel.findOne({ email: req.body.email });
 
-    if(!user){
+    if (!user) {
         return res.send("Invalid Email or Password");
     }
 
-    let result = await bcrypt.compare(req.body.password,user.password);
+    let result = await bcrypt.compare(req.body.password, user.password);
 
-    if(result){
+    if (result) {
 
-        let token = jwt.sign({email:user.email,id:user._id},"secretkey");
-        res.cookie("token",token);
+        let token = jwt.sign(
+            { email: user.email, id: user._id },
+            "secretkey"
+        );
+
+        res.cookie("token", token);
 
         res.redirect("/read");
     }
-    else{
+    else {
         res.send("Invalid Email or Password");
     }
 
 });
 
 
-app.get("/logout",(req,res)=>{
-    res.cookie("token","");
+//logout
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", "");
     res.redirect("/login");
 });
 
+//dashboard
 
 app.get("/read", isLoggedIn, async (req, res) => {
+
     let users = await userModel.find();
+
     res.render("read", { users });
+
 });
 
+
+//create user
 
 app.post("/create", isLoggedIn, async (req, res) => {
 
@@ -118,20 +141,33 @@ app.post("/create", isLoggedIn, async (req, res) => {
     });
 
     res.redirect("/read");
+
 });
 
+
+//user delete
 
 app.get("/delete/:userid", isLoggedIn, async (req, res) => {
+
     await userModel.findOneAndDelete({ _id: req.params.userid });
+
     res.redirect("/read");
+
 });
 
+
+//user edit
 
 app.get("/edit/:userid", isLoggedIn, async (req, res) => {
+
     let editid = await userModel.findOne({ _id: req.params.userid });
+
     res.render("edit", { editid });
+
 });
 
+
+//user update 
 
 app.post("/edit/:userid", isLoggedIn, async (req, res) => {
 
@@ -143,4 +179,5 @@ app.post("/edit/:userid", isLoggedIn, async (req, res) => {
     );
 
     res.redirect("/read");
+
 });
